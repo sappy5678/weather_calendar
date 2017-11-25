854006f8ab969461e85fa3233331b438b7d758b5
sappy5678 <sappy5678@gmail.com>
2017-11-25 21:20:18 +0800

    Test AWS Auto Upgrade

--- Backend/weather_calendar.py
+++ Backend/weather_calendar.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<(_ _)> Yeeeeee大大好神<(_ _)>!!! <br> <(_ _)> yeee大大千秋萬載 一統江湖 <(_ _)> <br><(_ _)> yeeee大大萬歲萬歲萬萬歲'
+    return '<(_ _)> Yeeeeee大大好神<(_ _)>!!! <br> <(_ _)> yeee大大千秋萬載 一統江湖 <(_ _)> <br><(_ _)> yeeee大大萬歲萬歲萬萬歲 <(_ _)>'
 
 
 if __name__ == '__main__':
