6d5c68a4872656b9e104dd7008c61845c8c3549d
david6686 <alucard6686@gmail.com>
2017-12-10 22:29:23 +0800

    davidVersion Upload main actvity(month_calendar) day_calendar(day calendar view)

---
+++ Frontend/weatherCalendar/app/src/main/java/com/example/weathercalendar/day_calendar.java
@@ -0,0 +1,230 @@
+package com.example.weathercalendar;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.framgia.library.calendardayview.CalendarDayView;
+import com.framgia.library.calendardayview.EventView;
+import com.framgia.library.calendardayview.PopupView;
+import com.framgia.library.calendardayview.data.IEvent;
+import com.framgia.library.calendardayview.data.IPopup;
+import com.framgia.library.calendardayview.decoration.CdvDecorationDefault;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+/**
+ * Created by user on 2017/12/8.
+ */
+//public class day_calendar extends AppCompatActivity {
+public class day_calendar extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
+
+    CalendarDayView dayView;
+
+    ArrayList<IEvent> events;
+    ArrayList<IPopup> popups;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.day_calendar_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
+
+        dayView = (CalendarDayView) findViewById(R.id.dayView);
+        dayView.setLimitTime(9, 22);
+
+        ((CdvDecorationDefault) (dayView.getDecoration())).setOnEventClickListener(
+                new EventView.OnEventClickListener() {
+                    @Override
+                    public void onEventClick(EventView view, IEvent data) {
+                        Log.e("TAG", "onEventClick:" + data.getName());
+                    }
+
+                    @Override
+                    public void onEventViewClick(View view, EventView eventView, IEvent data) {
+                        Log.e("TAG", "onEventViewClick:" + data.getName());
+                        if (data instanceof Event) {
+                            // change event (ex: set event color)
+                            dayView.setEvents(events);
+                        }
+                    }
+                });
+
+        ((CdvDecorationDefault) (dayView.getDecoration())).setOnPopupClickListener(
+                new PopupView.OnEventPopupClickListener() {
+                    @Override
+                    public void onPopupClick(PopupView view, IPopup data) {
+                        Log.e("TAG", "onPopupClick:" + data.getTitle());
+                    }
+
+                    @Override
+                    public void onQuoteClick(PopupView view, IPopup data) {
+                        Log.e("TAG", "onQuoteClick:" + data.getTitle());
+                    }
+
+                    @Override
+                    public void onLoadData(PopupView view, ImageView start, ImageView end,
+                                           IPopup data) {
+                        start.setImageResource(R.drawable.avatar);
+                    }
+                });
+
+
+        events = new ArrayList<>();
+
+        {
+            int eventColor = ContextCompat.getColor(this, R.color.eventColor);
+            Calendar timeStart = Calendar.getInstance();
+            timeStart.set(Calendar.HOUR_OF_DAY, 11);
+            timeStart.set(Calendar.MINUTE, 0);
+            Calendar timeEnd = (Calendar) timeStart.clone();
+            timeEnd.set(Calendar.HOUR_OF_DAY, 15);
+            timeEnd.set(Calendar.MINUTE, 30);
+            Event event = new Event(1, timeStart, timeEnd, "Event", "Hockaido", eventColor);
+
+            events.add(event);
+        }
+
+        {
+            int eventColor = ContextCompat.getColor(this, R.color.eventColor1);
+            Calendar timeStart = Calendar.getInstance();
+            timeStart.set(Calendar.HOUR_OF_DAY, 18);
+            timeStart.set(Calendar.MINUTE, 0);
+            Calendar timeEnd = (Calendar) timeStart.clone();
+            timeEnd.set(Calendar.HOUR_OF_DAY, 20);
+            timeEnd.set(Calendar.MINUTE, 30);
+            Event event = new Event(1, timeStart, timeEnd, "Another event", "Hockaido", eventColor);
+
+            events.add(event);
+        }
+
+        popups = new ArrayList<>();
+
+        {
+            Calendar timeStart = Calendar.getInstance();
+            timeStart.set(Calendar.HOUR_OF_DAY, 12);
+            timeStart.set(Calendar.MINUTE, 0);
+            Calendar timeEnd = (Calendar) timeStart.clone();
+            timeEnd.set(Calendar.HOUR_OF_DAY, 13);
+            timeEnd.set(Calendar.MINUTE, 30);
+
+            Popup popup = new Popup();
+            popup.setStartTime(timeStart);
+            popup.setEndTime(timeEnd);
+            popup.setImageStart("http://sample.com/image.png");
+            popup.setTitle("event 1 with title");
+            popup.setDescription("Yuong alsdf");
+            popups.add(popup);
+        }
+
+        {
+            Calendar timeStart = Calendar.getInstance();
+            timeStart.set(Calendar.HOUR_OF_DAY, 20);
+            timeStart.set(Calendar.MINUTE, 0);
+            Calendar timeEnd = (Calendar) timeStart.clone();
+            timeEnd.set(Calendar.HOUR_OF_DAY, 22);
+            timeEnd.set(Calendar.MINUTE, 0);
+
+            Popup popup = new Popup();
+            popup.setStartTime(timeStart);
+            popup.setEndTime(timeEnd);
+            popup.setImageStart("http://sample.com/image.png");
+            popup.setTitle("event 2 with title");
+            popup.setDescription("Yuong alsdf");
+            popups.add(popup);
+        }
+
+        dayView.setEvents(events);
+        dayView.setPopups(popups);
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
+
