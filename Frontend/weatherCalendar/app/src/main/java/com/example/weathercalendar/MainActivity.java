581b688e52c3330345a585ebea6e957239fc949a
david6686 <alucard6686@gmail.com>
2017-12-16 19:36:53 +0800

    comment coflict import  Please enter the commit message for your changes. Lines starting

--- Frontend/weatherCalendar/app/src/main/java/com/example/weathercalendar/MainActivity.java
+++ Frontend/weatherCalendar/app/src/main/java/com/example/weathercalendar/MainActivity.java
@@ -8,12 +8,9 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-<<<<<<< HEAD
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-=======
+//import android.support.v4.app.ActivityCompat;
+//import android.support.v4.content.ContextCompat;
 
->>>>>>> 8d38d370e8dd13180e4f5e89ffcdbd348a568bbf
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
